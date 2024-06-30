c2bd1795e6a6f72a147a3847d94c5eca6bc319a5
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2024-06-30 06:41:46 +0000

    if-else basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+// if-else
+
+const isUserLoggedIn = true;
+
+if(2=="2"){
+    // console.log(`lol`);
+}
+//2=="2" will return true.
+if(2==="2"){
+    console.log(`lol`);
+}// 2 === "2" will return false as it aslo checks datatypes.
+else{
+    console.log("Yaay");
+}
+
+if(true) console.log("hello"); // implicit scope
