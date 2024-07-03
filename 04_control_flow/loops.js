8791dd33fa43a5cb4cf09dbcc4771bafbfbe91d1
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2024-07-03 03:41:56 +0000

    Loops

---
+++ 04_control_flow/loops.js
@@ -0,0 +1,16 @@
+arr = [1,3,4,5,6,3,2]
+for (const num of arr) {
+    // console.log(arr[num]);
+}
+
+const greet = "Hello World"
+for(const i of greet){
+    if(i==" ") continue;
+    // else console.log(i);
+}
+
+const map = new Map()
+map.set('IN' , "India")
+map.set('AUS' , "Australia")
+
+console.log(map);
