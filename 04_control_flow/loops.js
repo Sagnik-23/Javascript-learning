5c822bc301dc56d0d14729a78aedd85be08e1528
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2024-07-05 13:59:26 +0000

    loops

--- 04_control_flow/loops.js
+++ 04_control_flow/loops.js
@@ -13,4 +13,78 @@ const map = new Map()
 map.set('IN' , "India")
 map.set('AUS' , "Australia")
 
-console.log(map);
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key + " Value:-" + value);
+}
+
+// for of obj
+const myObj = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key,value] of myObj) {
+//     console.log(key + value);//TypeError: myObj is not iterable
+// }
+
+//For in loop
+const myobj2 = {
+    js: "javascript",
+    cpp: "c++",
+    py: "python"
+}
+
+for (const key in myobj2) {
+    // console.log(`${key} is the shortcut of ${myobj2[key]}`);
+}
+
+//Array
+
+const programming = ["js","py","cpp","java"]
+for (const key in programming) {
+    // console.log(key + ":-" + programming[key]);
+}
+
+//in map for in loop don't run//
+
+//For each loop
+
+const coding = ["js","ruby","python","cpp","java"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+function printMe(item){
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item,index,arr);
+// } )
+
+const myArray = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myArray.forEach( (item) => {
+    console.log(item.languageName);
+} )
